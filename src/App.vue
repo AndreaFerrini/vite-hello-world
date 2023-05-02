f26c4d87c7c0a1d141edeb6bf82113650491cf4d
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-05-02 23:46:02 +0200

    creazione primo messaggio

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+  import PrimoComponente from './components/PrimoComponente.vue';
+
+  export default{
+    name: "App",
+    components: {
+      PrimoComponente
+    }
+  }
+  
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>La Mia Prima App Con Vite!</h1>
+  <PrimoComponente/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+  template{
+    background-color: rgb(27, 27, 27);
+  }
+
+  h1{
+    color: gold;
+  }
+
 </style>
---
+++ src/components/PrimoComponente.vue
@@ -0,0 +1,34 @@
+<script>
+
+  export default{
+    name: "PrimoComponente",
+    data(){
+      return{
+       
+      }
+    },
+    methods: {
+
+    },
+  }
+
+</script>
+
+<template>
+    <div>
+        <h1>La Mia Prima App Con Vite!</h1>
+    </div>
+
+</template>
+
+<style scoped>
+
+    template{
+        background-color: rgb(27, 27, 27);
+    }
+
+    h1{
+        color: gold;
+    }
+  
+</style>
